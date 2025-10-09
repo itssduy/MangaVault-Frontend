6758e1e657e3ba1865bfb80c3e9029dfdcf6caa8
Duy <duypersonal512@gmail.com>
2025-10-09 12:37:39 -0500

    Code homePage wireframe

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,12 +1,19 @@
 import '../styles/home.css'
 
 const home = ()=>{
+    const mangas = [1,2,3,4,5,6,7,8,9,10]
     return (
         <>
             <main className='homeContainer'>
 
                 <div className='mangas'>
-                    <p>Mangas</p>
+                    <ul className='mangasList'>
+                        {mangas.map((i)=>{
+                            return (
+                                <li className='manga'>{i}</li>
+                            )
+                        })}
+                    </ul>
                 </div>
                 <div className='trending'>
                     <p>Trending</p>
